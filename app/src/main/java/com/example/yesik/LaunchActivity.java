c095e71e0506018f7905e2c83d74d639cb4e440f
Naki <nhs120417@gmail.com>
2023-03-30 18:03:29 +0900

    통화(CALL_PHONE), 카메라(CAMERA), 저장공간(READ/WRITE_EXTERNAL_STAORAGE) 권한 설정

---
+++ app/src/main/java/com/example/yesik/LaunchActivity.java
@@ -0,0 +1,99 @@
+package com.example.yesik;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!hasPermissions(PERMISSIONS)) {
+                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+            else {
+                Intent toMain = new Intent(LaunchActivity.this, MainActivity.class);
+                startActivity(toMain);
+
+                finish();
+            }
+        }
+    }
+
+    static final int PERMISSIONS_REQUEST_CODE = 1000;
+    String[] PERMISSIONS = {android.Manifest.permission.CALL_PHONE, android.Manifest.permission.CAMERA, android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    private boolean hasPermissions(String[] permissions) {
+        int result;
+
+        for (String permission : permissions) {
+
+            result = ContextCompat.checkSelfPermission(this, permission);
+
+            if (result == PackageManager.PERMISSION_DENIED) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch(requestCode) {
+            case PERMISSIONS_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    boolean cameraPermissionAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean diskPermissionAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    boolean callPermissionAccepted = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+
+                    if (!cameraPermissionAccepted || !diskPermissionAccepted || !callPermissionAccepted) {
+                        showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
+                    }
+                    else {
+                        Intent toMain = new Intent(LaunchActivity.this, MainActivity.class);
+                        startActivity(toMain);
+
+                        finish();
+                    }
+                }
+                break;
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void showDialogForPermission(String msg) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(LaunchActivity.this);
+        builder.setTitle("알림");
+        builder.setMessage(msg);
+        builder.setCancelable(false);
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                finish();
+            }
+        });
+        builder.create().show();
+    }
+
+}
--- app/src/main/java/com/example/yesik/MainActivity.java
+++ app/src/main/java/com/example/yesik/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.yesik;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
         moveLogin(1);
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+        }
     }
 
     private void moveLogin(int sec) {
