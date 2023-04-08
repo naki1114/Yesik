378dc88defd78ce678baa7eb12f9898daf45a842
Naki <nhs120417@gmail.com>
2023-04-08 16:21:03 +0900

    BitmapConverter Class 추가 (Bitmap to String / String to Bitmap)

---
+++ app/src/main/java/com/example/yesik/BitmapConverter.java
@@ -0,0 +1,29 @@
+package com.example.yesik;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class BitmapConverter {
+    public String BitmapToString(Bitmap bitmap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 70, baos);
+        byte[] bytes = baos.toByteArray();
+        String temp = Base64.encodeToString(bytes, Base64.DEFAULT);
+
+        return temp;
+    }
+
+    public Bitmap StringToBitmap(String encodedString) {
+        try {
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+}
