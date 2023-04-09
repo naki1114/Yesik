dbc7ed19f11a18dd956656d4412b17aaa94afd8b
Naki <nhs120417@gmail.com>
2023-04-09 19:18:55 +0900

    식당 등록 페이지 Spinner 사용

--- app/src/main/java/com/example/yesik/Restaurant_Register.java
+++ app/src/main/java/com/example/yesik/Restaurant_Register.java
@@ -1,22 +1,42 @@
 package com.example.yesik;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Restaurant_Register extends AppCompatActivity {
 
     String tag;
 
+    EditText nameInput;
+    EditText placeInput;
+
+    Button restaurantRegistButton;
+    Button imageRegistButton;
+
+    ImageButton getImageButton;
+
+    Spinner imageSelectSpinner;
+
+    RecyclerView restaurantInnerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_register);
-
-        tag = "식당 등록 페이지";
         Log.v(tag, "onCreate() 호출됨");
 
+        initializing();
     }
 
     @Override
@@ -29,6 +49,26 @@ public class Restaurant_Register extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.v(tag, "onResume() 호출됨");
+
+        imageSelectSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 1) {
+                    Toast.makeText(getApplicationContext(), "로고", Toast.LENGTH_SHORT).show();
+                }
+                else if (position == 2) {
+                    Toast.makeText(getApplicationContext(), "내부 전경", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "사진 분류를 선택하세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     @Override
@@ -47,6 +87,30 @@ public class Restaurant_Register extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.v(tag, "onDestroy()");
+    }
+
+    // Life Cycle Finish
+
+    // Custom Method
+
+    public void initializing() {
+        tag = "식당 등록 페이지";
+
+        nameInput = findViewById(R.id.nameInput);
+        placeInput = findViewById(R.id.placeInput);
+
+        restaurantRegistButton = findViewById(R.id.restaurantRegistButton);
+        imageRegistButton = findViewById(R.id.imageRegistButton);
+
+        getImageButton = findViewById(R.id.getImageButton);
+
+        imageSelectSpinner = findViewById(R.id.imageSelectSpinner);
+
+        restaurantInnerView = findViewById(R.id.restaurantInnerView);
+    }
+
+    public void selectSpinner() {
+
     }
 
 }
