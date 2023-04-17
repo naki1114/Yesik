9397684ad2547aae26d571fd386921d857de575d
Naki <nhs120417@gmail.com>
2023-04-17 12:46:26 +0900

    식당 선택 화면 식당 분류별 구분 완료

--- app/src/main/java/com/example/yesik/Select_Restaurant.java
+++ app/src/main/java/com/example/yesik/Select_Restaurant.java
@@ -1,22 +1,55 @@
 package com.example.yesik;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class Select_Restaurant extends AppCompatActivity {
 
     String tag;
 
+    BitmapConverter converter;
+
+    SharedPreferences getRestaurantInfo;
+    SharedPreferences getLogoImage;
+
+    RecyclerView restaurantListRecyclerView;
+    RestaurantAdapter restaurantAdapter;
+
+    String[] allRestaurantUserIDList;
+    String[] allRestaurantNameList;
+    String[] allRestaurantPlaceList;
+    String[] allRestaurantDivisionList;
+
+    ArrayList<RestaurantItem> restaurantList;
+
+    ArrayList<String> restaurantLogoList;
+    ArrayList<String> restaurantNameList;
+    ArrayList<String> restaurantPlaceList;
+    ArrayList<String> restaurantDivisionList;
+
+    String division;
+
+    int restaurantUserCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_restaurant);
-
-        tag = "식당 선택 페이지";
         Log.v(tag, "onCreate() 호출됨");
 
+        initializing();
+        divideRestaurant();
+        restaurantListView();
     }
 
     @Override
@@ -47,6 +80,73 @@ public class Select_Restaurant extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.v(tag, "onDestroy()");
+    }
+
+    // Life Cycle Finish
+
+    // Custom Method
+
+    public void initializing() {
+        tag = "식당 선택 페이지";
+
+        restaurantListRecyclerView = (RecyclerView) findViewById(R.id.restaurantRecyclerView);
+        restaurantListRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        restaurantListRecyclerView.addItemDecoration(new DividerItemDecoration(this, 1));
+
+        Intent getDivision = getIntent();
+
+        division = getDivision.getStringExtra("division");
+
+        converter = new BitmapConverter();
+
+        restaurantList = new ArrayList<>();
+
+        restaurantLogoList = new ArrayList<>();
+        restaurantNameList = new ArrayList<>();
+        restaurantPlaceList = new ArrayList<>();
+        restaurantDivisionList = new ArrayList<>();
+
+        restaurantAdapter = new RestaurantAdapter(restaurantList);
+
+        getRestaurantInfo = getSharedPreferences("UserInfoSplit", MODE_PRIVATE);
+        getLogoImage = getSharedPreferences("RestaurantImage", MODE_PRIVATE);
+    }
+
+    public void divideRestaurant() {
+        allRestaurantUserIDList = getRestaurantInfo.getString("Restaurant User ID", "").split("⊙");
+        allRestaurantNameList = getRestaurantInfo.getString("Restaurant Name", "").split("⊙");
+        allRestaurantPlaceList = getRestaurantInfo.getString("Restaurant Place", "").split("⊙");
+        allRestaurantDivisionList = getRestaurantInfo.getString("Restaurant Division", "").split("⊙");
+        restaurantUserCount = getRestaurantInfo.getInt("Restaurant User Count", 0);
+
+        if (division.equals("ALL")) {
+            for (int count = 1; count <= restaurantUserCount; count++) {
+                restaurantNameList.add(allRestaurantNameList[count]);
+                restaurantPlaceList.add(allRestaurantPlaceList[count]);
+                restaurantDivisionList.add(allRestaurantDivisionList[count]);
+                restaurantLogoList.add(getLogoImage.getString(allRestaurantUserIDList[count] + " Logo", ""));
+            }
+        }
+        else {
+            for (int count = 0; count <= restaurantUserCount; count++) {
+                if (allRestaurantDivisionList[count].equals(division)) {
+                    restaurantNameList.add(allRestaurantNameList[count]);
+                    restaurantPlaceList.add(allRestaurantPlaceList[count]);
+                    restaurantDivisionList.add(allRestaurantDivisionList[count]);
+                    restaurantLogoList.add(getLogoImage.getString(allRestaurantUserIDList[count] + " Logo", ""));
+                }
+            }
+        }
+    }
+
+    public void restaurantListView() {
+        for (int count = 0; count < restaurantNameList.size(); count++) {
+            RestaurantItem restaurantListItem = new RestaurantItem(converter.StringToBitmap(restaurantLogoList.get(count)), restaurantNameList.get(count), restaurantPlaceList.get(count));
+
+            restaurantAdapter.addData(restaurantListItem);
+        }
+        restaurantListRecyclerView.setAdapter(restaurantAdapter);
+        restaurantAdapter.notifyDataSetChanged();
     }
 
 }
