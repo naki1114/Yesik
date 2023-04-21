68d7f60496a330c58cb7000cb35d41e3e0cb1eca
Naki <nhs120417@gmail.com>
2023-04-21 22:38:14 +0900

    Kakao Map에 원하는 주소값으로 marker 표시

--- app/src/main/java/com/example/yesik/Reserve_Time.java
+++ app/src/main/java/com/example/yesik/Reserve_Time.java
@@ -1,14 +1,19 @@
 package com.example.yesik;
 
+import static net.daum.mf.map.api.MapPOIItem.MarkerType.RedPin;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -18,10 +23,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class Reserve_Time extends AppCompatActivity {
 
+//    private static final MapPoint MARKER_POINT = ;
     String tag;
 
     LinearLayout restaurantInfoLayout;
@@ -227,12 +239,59 @@ public class Reserve_Time extends AppCompatActivity {
     public void setMapView() {
         mapView = new MapView(this);
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.mapViewLayout);
-        mapViewContainer.addView(mapView);
+        mapViewLayout = findViewById(R.id.mapViewLayout);
+        mapViewLayout.addView(mapView);
 
+        double restaurantLatitude = findGeoPoint(getApplicationContext(), "경기도 수원시 영통구 권광로 260번길 36").getLatitude();
+        double restaurantLongitude = findGeoPoint(getApplicationContext(), "경기도 수원시 영통구 권광로 260번길 36").getLongitude();
+
+        // 중심점 변경
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(restaurantLatitude, restaurantLongitude), true);
+        // 줌 레벨 변경
         mapView.setZoomLevel(1,true);
+        // 줌 인
+        mapView.zoomIn(true);
+        // 줌 아웃
+        mapView.zoomOut(true);
 
-        mapView.setCurrentLocationEventListener((MapView.CurrentLocationEventListener) this);
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(restaurantLatitude, restaurantLongitude);
+        MapPOIItem marker = new MapPOIItem();
+
+        // 클릭 시 나오는 값
+        marker.setItemName("응급실 떡볶이");
+        // 왜 있는지 모르겠음
+        marker.setTag(0);
+        // 좌표를 입력받아 현 위치로 출력
+        marker.setMapPoint(MARKER_POINT);
+        // (클릭 전) 기본으로 제공하는 BluPin 마커 모양의 색
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        // (클릭 후) 마커를 클릭했을 때, 기본으로 제공하는 RedPin 마커 모양의 색
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        // 지도화면 위에 추가되는 아이콘을 추가하기 위한 호출 (말풍선 모양)
+        mapView.addPOIItem(marker);
+    }
+
+    public static Location findGeoPoint(Context mcontext, String address) {
+        Location loc = new Location("");
+        Geocoder coder = new Geocoder(mcontext);
+        List<Address> addr = null;// 한좌표에 대해 두개이상의 이름이 존재할수있기에 주소배열을 리턴받기 위해 설정
+
+        try {
+            addr = coder.getFromLocationName(address, 5);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }// 몇개 까지의 주소를 원하는지 지정 1~5개 정도가 적당
+        if (addr != null) {
+            for (int i = 0; i < addr.size(); i++) {
+                Address lating = addr.get(i);
+                double lat = lating.getLatitude(); // 위도가져오기
+                double lon = lating.getLongitude(); // 경도가져오기
+                loc.setLatitude(lat);
+                loc.setLongitude(lon);
+            }
+        }
+        return loc;
     }
 
 }
